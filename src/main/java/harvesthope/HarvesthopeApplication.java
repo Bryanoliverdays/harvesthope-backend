92497c79b36a6715cc97a845b036c03a2d615301
Xsbryx <rm99668@estudante.fieb.edu.br>
2026-09-21 10:46:40 -0300

    criacao backend harvesthope

---
+++ src/main/java/harvesthope/HarvesthopeApplication.java
@@ -0,0 +1,13 @@
+package harvesthope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvesthopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvesthopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/harvesthope/HarvesthopeApplicationTests.java
@@ -0,0 +1,13 @@
+package harvesthope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvesthopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
